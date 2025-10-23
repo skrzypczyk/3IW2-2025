ce5eab0a94161a17b4d2c45f105f6a1e1b068284
bilalTaoufik0 <bll.taoufik@gmail.com>
2025-10-23 09:39:17 +0200

    save

---
+++ www/TP2.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * Un tableau HTML avec la liste des users
+ * et un bouton par ligne pour supprimer un user
+ * avec un confirm JS pour confirmer la suppression en BDD
+ * et un mail pour prévenir le user
+ */
